5b68f16bca0ff071de67ddf5b54b62bbece6add1
Derek Marion <derek.j.marion@gmail.com>
2023-10-11 07:30:50 -0700

    Add Bird, Reptile, Primate, and Marsupial subclasses

--- zoo_manager.py
+++ zoo_manager.py
@@ -13,3 +13,28 @@ class Mammal(Animal):
     def give_birth(self):
         print(f"{self._name} has given birth!")
     
+class Bird(Animal):
+    def __init__(self, name, species, wingspan) -> None:
+        super().__init__(name, species="Bird")
+        self._wingspan = wingspan
+
+class Reptile(Animal):
+    def __init__(self, name, species) -> None:
+        super().__init__(name, species)
+
+    def bask_in_sun(self):
+        print(f"{self._name} is basking in the sun")
+
+class Primate(Mammal):
+    def __init__(self, name, species) -> None:
+        super().__init__(name, species)
+
+    def climb_trees(self):
+        print(f"{self._name} is climbing a tree!")
+
+class Marsupial(Mammal):
+    def __init__(self, name, species) -> None:
+        super().__init__(name, species)
+
+    def carry_baby(self):
+        print(f"{self._name} is carrying its baby")
